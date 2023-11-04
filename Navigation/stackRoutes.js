b38b0837bc874e5aa549ed5210ff8f2b647d2a87
lupitasgm <93928511+lupitasgm@users.noreply.github.com>
2023-11-04 01:04:50 -0600

    Update stackRoutes.js

--- Navigation/stackRoutes.js
+++ Navigation/stackRoutes.js
@@ -1,9 +1,15 @@
 import Home from '../Views/Home';
 import Iniciosesion from '../Views/Iniciosesion';
 import Registro from '../Views/Registro';
+import Pacientes from '../Views/Pacientes';
+import UpdatePacientes from '../Views/UpdatePacientes';
+import RegistrarPacientes from '../Views/RegistrarPaciente'
 
 export default {
     Home,
     Iniciosesion,
     Registro,
+    Pacientes,
+    UpdatePacientes,
+    RegistrarPacientes,
 }
